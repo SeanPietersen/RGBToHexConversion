3d4fc540756a726f68371f38a8560e6d7b691a2a
Sean Pietersen <seanpietersen7@gmail.com>
2022-05-10 11:53:10 +0200

    Feat: Added Test

---
+++ RGBToHexConversion/RGBToHexConversion.Test/ConvertFromRGBToHexTest.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+namespace RGBToHexConversion.Test
+{
+    public class ConvertFromRGBToHexTest
+    {
+        [Theory]
+        [InlineData(255, 255, 255, "FFFFFF")]
+        [InlineData(255, 255, 300, "FFFFFF")]
+        [InlineData(0, 0, 0, "000000")]
+        [InlineData(148, 0, 211, "9400D3")]
+        [InlineData(148, -20, 211, "9400D3")]
+        [InlineData(144, 195, 212, "90C3D4")]
+        [InlineData(212, 53, 12, "D4350C")]
+
+        public void RGBToHexConverterTest_successful(int r, int g, int b, string expected)
+        {
+            //Arrange
+            IConvertFromRGBToHex convertFromRGBToHex = new ConvertFromRGBToHex();
+
+            //Act
+            var actual = convertFromRGBToHex.RGBToHexConverter(r, g, b);
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
